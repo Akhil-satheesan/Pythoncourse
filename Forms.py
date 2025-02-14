a2afe0bd91bc8217b7d1fd50c7fbe87508fdf2f7
Akhil-satheesan <akhilsatheesan557@gmail.com>
2025-02-14 14:08:54 +0530

    Create Forms.py

---
+++ Forms.py
@@ -0,0 +1,8 @@
+
+from django import forms
+from.models import Cake
+
+class CakeForm(forms.ModelForm):
+    class Meta:
+        model=Cake
+        fields=['cake_name','cake_flavour','cake_desc','cake_price','cake_img'] forms.py
